eca44544ff222b38f140961e65dfc8224c3c9504
shunichitanaka <shunichi.tanaka@agoop.co.jp>
2016-08-17 12:22:53 +0900

    commit3

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 田中舜一 on 2016/08/17.
+//  Copyright © 2016年 田中舜一. All rights reserved.
+//
+
+import Foundation
+
+struct CommonConst {
+    static let PostPATH = "posts"
+    static let DisplayNameKey = "DISPLAYNAME"
+}
--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import ESTabBarController
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
@@ -14,12 +17,61 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        //aiu
+        //setupTab()
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setupTab(){
+        
+        let tabBarController = ESTabBarController(tabIconNames: ["home","camera","setting"])
+        
+        tabBarController.selectedColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
+        tabBarController.buttonsBackgroundColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
+        
+        
+        // 作成したESTabBarControllerを親のViewController（＝self）に追加する
+        addChildViewController(tabBarController)
+        view.addSubview(tabBarController.view)
+        tabBarController.view.frame = view.bounds
+        tabBarController.didMoveToParentViewController(self)
+        
+        // タブをタップした時に表示するViewControllerを設定する
+        let homeViewController = storyboard?.instantiateViewControllerWithIdentifier("Home")
+        let settingViewController = storyboard?.instantiateViewControllerWithIdentifier("Setting")
+        
+        tabBarController.setViewController(homeViewController, atIndex: 0)
+        tabBarController.setViewController(settingViewController, atIndex: 2)
+        
+        // 真ん中のタブはボタンとして扱う
+        tabBarController.highlightButtonAtIndex(1)
+        tabBarController.setAction({
+            
+            // ボタンが押されたらImageViewControllerをモーダルで表示する
+            let imageViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ImageSelect")
+            self.presentViewController(imageViewController!, animated: true, completion: nil)
+            }, atIndex: 1)
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // currentUserがnilならログインしていない
+        if FIRAuth.auth()?.currentUser != nil {
+            // ログインしているときの処理
+            setupTab()
+        } else {
+            // ログインしていないときの処理
+            dispatch_async(dispatch_get_main_queue()) {
+                let loginViewController = self.storyboard?.instantiateViewControllerWithIdentifier("Login")
+                self.presentViewController(loginViewController!, animated: true, completion: nil)
+            }
+        }
     }
 
 
